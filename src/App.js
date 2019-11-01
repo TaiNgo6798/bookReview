6038fcaeef8eff4348a9bcce691bef9f5b19fa11
taingo <taingo6798@gmail.com>
2019-11-01 19:35:59 +0700

    create

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { routers } from './configs'
+import './App.css'
+// import pages
+
+
+const App = (props) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Switch>
+            {routers.map((route, idx) => (
+              <Route
+                key={idx}
+                exact={route.exact}
+                path={route.path}
+                render={() => {
+                  const Component = React.lazy(() => import(`./pages/${route.component}`))
+                  return <Component />
+                }}
+              />
+            ))}
+            <Redirect to='/' />
+          </Switch>
+        </Router>
+    </Suspense>
+  );
+}
+
+export default App
---
+++ src/components/nav/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import { Input, Badge, Icon } from 'antd';
+
+
+import './index.scss'
+
+const { Search } = Input;
+
+function Index() {
+
+  const [ messageCount, setMessageCount ] = useState(0)
+
+  const loadNotify = () => {
+    let myCount = setInterval(() => {
+      setMessageCount(messageCount => messageCount + 1)
+    }, 1000);
+  }
+  useEffect(() => {
+    loadNotify()
+  }, [])
+
+  return (
+    <div className='nav'>
+      <div className='container'>
+        <div className='logo'></div>
+        <div className='searchBar'>
+          <Search
+            placeholder="input search text"
+            onSearch={value => console.log(value)}
+            style={{ width: 200 }}
+          />
+        </div>
+        <div className='notify'>
+          <Badge count={0}>
+          <Icon type="usergroup-delete" className='icon'/>
+          </Badge>
+          <Badge count={messageCount}>
+          <Icon type="mail"  className='icon'/>
+          </Badge>
+          <Badge count={5}>
+          <Icon type="bell" className='icon'/>
+          </Badge>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Index
